e6d33fb107e3b33054eef45f9487390a9211d67a
Richard Neumann <r.neumann@homeinfo.de>
2023-02-21 13:48:30 +0100

    Introduce training task

--- dsla/selection_method_run.py
+++ dsla/selection_method_run.py
@@ -2,11 +2,11 @@
 
 from typing import NamedTuple
 
-from dsla.event import Event
 from dsla.nasa_tlx import NASA_TLX
 from dsla.selection_method import SelectionMethod
 from dsla.study_task import StudyTask
 from dsla.system_usability_scale import SystemUsabilityScale
+from dsla.training_task import TrainingTask
 
 
 __all__ = ['SelectionMethodRun']
@@ -16,7 +16,7 @@ class SelectionMethodRun(NamedTuple):
     """Selection method run."""
 
     selection_method: SelectionMethod
-    training: list[Event]
+    training: list[TrainingTask]
     tasks: list[StudyTask]
     system_usability_scale: SystemUsabilityScale
     nasa_tlx: NASA_TLX
---
+++ dsla/training_task.py
@@ -0,0 +1,9 @@
+"""A Training task."""
+
+from dsla.event import Event
+
+
+__all__ = ['TrainingTask']
+
+
+TrainingTask = list[Event]
