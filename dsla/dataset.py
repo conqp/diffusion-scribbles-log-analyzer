d69134516405fa5d30b88f4773b47852d669bcd9
Richard Neumann <r.neumann@homeinfo.de>
2023-02-21 18:03:09 +0100

    Add missing dataset

--- dsla/dataset.py
+++ dsla/dataset.py
@@ -9,6 +9,10 @@ __all__ = ['Dataset']
 class Dataset(str, Enum):
     """Available datasets."""
 
+    # Training
+    R15 = '10_R15'
+
+    # Study
     GAUSSIAN = '1_Gaussian'
     PATH_BASED = '2_pathbased'
     CRIM_RAD = '3_CRIM_RAD'
