55dde70ac2a244df9711eb3f433af0ea8612ea0c
Richard Neumann <r.neumann@homeinfo.de>
2023-02-26 14:29:03 +0100

    Add command line switches for different data

--- dsla/main.py
+++ dsla/main.py
@@ -5,7 +5,7 @@ from json import dumps
 from logging import DEBUG, INFO, basicConfig
 from pathlib import Path
 
-from dsla.datastructures import ParticipantData
+from dsla.datastructures import Dataset, ParticipantData
 from dsla.plots import plot_age_distribution, plot_self_assessment_distribution
 from dsla.reader import read
 from dsla.statistics import average_demographics
@@ -22,6 +22,42 @@ def get_args(description: str = __doc__) -> Namespace:
     parser = ArgumentParser(description=description)
     parser.add_argument('file', type=Path, nargs='+')
     parser.add_argument('-d', '--debug', action='store_true')
+    parser.add_argument(
+        '-B', '--brushing-methods', action='store_true',
+        help='print brushing methods statistics'
+    )
+    parser.add_argument(
+        '-D', '--demographics', action='store_true',
+        help='print demographic statistics'
+    )
+    parser.add_argument(
+        '-N', '--nasa-tlx', action='store_true',
+        help='print NASA-TLX statistics'
+    )
+    parser.add_argument(
+        '-P', '--participants', action='store_true',
+        help='print amount of participants'
+    )
+    parser.add_argument(
+        '-S', '--self-assessment', action='store_true',
+        help='print self-assessment statistics'
+    )
+    parser.add_argument(
+        '-T', '--training', action='store_true',
+        help='print training statistics'
+    )
+    parser.add_argument(
+        '-U', '--system-usability-scale', action='store_true',
+        help='print System Usability Scale statistics'
+    )
+    parser.add_argument(
+        '-p', '--plot', action='store_true',
+        help='plot data if applicable'
+    )
+    parser.add_argument(
+        '-x', '--exclude-dataset', type=Dataset, nargs='*',
+        help='exclude datasets from statistics'
+    )
     return parser.parse_args()
 
 
@@ -30,43 +66,64 @@ def main():
 
     args = get_args()
     basicConfig(level=DEBUG if args.debug else INFO)
-    experiments = [
-        ParticipantData.from_items(read(file)) for file in args.file
-    ]
-
-    print('Participants:', len(experiments))
-
-    for experiment in experiments:
-        if not experiment.participant.vision_ok:
-            print('Uncorrected vision:', experiment.participant)
-
-    print(
-        'Average demographics:',
-        dumps(average_demographics(experiments), indent=2)
-    )
-    print(
-        'Self-assessment:',
-        dumps(self_assessment_distribution(experiments), indent=2)
-    )
-    print(
-        'Training:',
-        dumps(training_runs(experiments), indent=2)
-    )
-    print(
-        'Scribbles:',
-        dumps(scribble_stats(experiments), indent=2)
-    )
-    print(
-        'System Usability Scale:',
-        dumps(average_sus(experiments), indent=2)
-    )
-    print(
-        'NASA-TLX:',
-        dumps(average_tlx(experiments), indent=2)
+    experiments = list(
+        filter(
+            lambda exp: exp.participant.vision_ok,
+            (ParticipantData.from_items(read(file)) for file in args.file)
+        )
     )
 
-    plot_age_distribution(experiments)
-    plot_self_assessment_distribution(experiments)
+    if args.participants:
+        print('Participants:', len(experiments))
+
+    if args.demographics:
+        print(
+            'Average demographics:',
+            dumps(average_demographics(experiments), indent=2)
+        )
+
+    if args.self_assessment:
+        print(
+            'Self-assessment:',
+            dumps(self_assessment_distribution(experiments), indent=2)
+        )
+
+    if args.training:
+        print(
+            'Training:',
+            dumps(training_runs(experiments), indent=2)
+        )
+
+    if args.brushing_methods:
+        print(
+            'Brushing methods:',
+            dumps(
+                scribble_stats(
+                    experiments,
+                    exclude_datasets={Dataset.RAD_PTRATIO}
+                ),
+                indent=2
+            )
+        )
+
+    if args.system_usability_scale:
+        print(
+            'System Usability Scale:',
+            dumps(average_sus(experiments), indent=2)
+        )
+
+    if args.nasa_tlx:
+        print(
+            'NASA-TLX:',
+            dumps(average_tlx(experiments), indent=2)
+        )
+
+    if args.plot:
+        if args.demographics:
+            plot_age_distribution(experiments)
+
+        if args.self_assessment:
+            plot_self_assessment_distribution(experiments)
 
 
 if __name__ == '__main__':
