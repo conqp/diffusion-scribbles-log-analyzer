f4faf119637dcf23014c63b03c40f9557968b196
Richard Neumann <r.neumann@homeinfo.de>
2023-02-26 11:30:49 +0100

    Add average summaries

--- dsla/statistics/scribble.py
+++ dsla/statistics/scribble.py
@@ -1,11 +1,12 @@
 """Scribble statistics."""
 
-from typing import Any
 from statistics import mean
+from typing import Any
 
 from dsla.datastructures import STUDY_DATASETS
 from dsla.datastructures import ParticipantData
 from dsla.datastructures import SelectionMethod
+from dsla.datastructures import Summary
 
 
 __all__ = ['scribble_stats']
@@ -21,6 +22,15 @@ def scribble_stats(experiments: list[ParticipantData]) -> dict[str, Any]:
                 for experiment in experiments
                 for run in experiment.runs
             ),
+            'summary': mean_summary(
+                [
+                    task.summary
+                    for experiment in experiments
+                    for run in experiment.runs
+                    for task in run.tasks
+                    if run.selection_method is method
+                ]
+            ),
             **{
                 dataset: {
                     'events': mean(
@@ -31,7 +41,41 @@ def scribble_stats(experiments: list[ParticipantData]) -> dict[str, Any]:
                         if run.selection_method is method
                         and task.dataset is dataset
                     ),
+                    'summary': mean_summary(
+                        [
+                            task.summary
+                            for experiment in experiments
+                            for run in experiment.runs
+                            for task in run.tasks
+                            if run.selection_method is method
+                            and task.dataset is dataset
+                        ]
+                    )
                 } for dataset in STUDY_DATASETS
             }
         } for method in SelectionMethod
+    }
+
+
+def mean_summary(summaries: list[Summary]) -> dict[str, Any]:
+    """Average task summaries."""
+
+    return {
+        'task_duration': mean(
+            summary.task_duration.total_seconds() for summary in summaries
+        ),
+        'draw_duration': mean(
+            summary.draw_duration.total_seconds() for summary in summaries
+        ),
+        'invert_count': mean(summary.invert_count for summary in summaries),
+        'undo_count': mean(summary.undo_count for summary in summaries),
+        'redo_count': mean(summary.redo_count for summary in summaries),
+        'reset_count': mean(summary.reset_count for summary in summaries),
+        'scribble_count': mean(
+            summary.scribble_count for summary in summaries
+        ),
+        'scribble_pixels': mean(
+            summary.scribble_pixels for summary in summaries
+        ),
+        'eraser_count': mean(summary.eraser_count for summary in summaries),
     }
