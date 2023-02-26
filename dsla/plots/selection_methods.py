676f20791d82b4e9f457db6ba2e74e4593d3460c
Richard Neumann <r.neumann@homeinfo.de>
2023-02-26 15:28:45 +0100

    Update iteration

--- dsla/plots/selection_methods.py
+++ dsla/plots/selection_methods.py
@@ -13,5 +13,5 @@ def plot_average_method_performance_on_plots(
     """Plot mean performance data per brushing method and scatter plot."""
 
     for method, method_stats in selection_method_stats(experiments).items():
-        for plot, scatter_plot_stats in method_stats['scatter_plots']:
+        for dataset, dataset_stats in method_stats['datasets'].items():
             raise NotImplementedError()
