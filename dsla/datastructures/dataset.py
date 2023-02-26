adfa3f7d2c70d9e58c4b2084b79c129ae6c776bc
Richard Neumann <r.neumann@homeinfo.de>
2023-02-26 10:05:50 +0100

    Add missing dataset
    While at it, order datasets

--- dsla/datastructures/dataset.py
+++ dsla/datastructures/dataset.py
@@ -9,10 +9,6 @@ __all__ = ['Dataset']
 class Dataset(str, Enum):
     """Available datasets."""
 
-    # Training
-    PERSONAL_HIKING_DATA = '7_personalHikingData'
-    R15 = '10_R15'
-    SIMPLIFIED_IRIS = '12_simplified.iris.data.set'
     # Study
     GAUSSIAN = '1_Gaussian'
     PATH_BASED = '2_pathbased'
@@ -20,3 +16,8 @@ class Dataset(str, Enum):
     RAD_PTRATIO = '4_RAD_PTRATIO'
     INDUS_NOX = '5_INDUS_NOX'
     AGE_DIS = '6_AGE_DIS'
+    # Training
+    PERSONAL_HIKING_DATA = '7_personalHikingData'
+    AGGREGATION = '8_Aggregation'
+    R15 = '10_R15'
+    SIMPLIFIED_IRIS = '12_simplified.iris.data.set'
