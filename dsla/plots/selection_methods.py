7f4af1692e30b9167c25c39f0f9ab796c49c818d
Richard Neumann <r.neumann@homeinfo.de>
2023-02-26 15:26:39 +0100

    Add boilerplate for per-plot method performance plotting

---
+++ dsla/plots/selection_methods.py
@@ -0,0 +1,17 @@
+"""Plot statistics on selection methods."""
+
+from dsla.datastructures import ParticipantData
+from dsla.statistics import selection_method_stats
+
+
+__all__ = ['plot_average_method_performance_on_plots']
+
+
+def plot_average_method_performance_on_plots(
+        experiments: list[ParticipantData]
+) -> None:
+    """Plot mean performance data per brushing method and scatter plot."""
+
+    for method, method_stats in selection_method_stats(experiments).items():
+        for plot, scatter_plot_stats in method_stats['scatter_plots']:
+            raise NotImplementedError()
