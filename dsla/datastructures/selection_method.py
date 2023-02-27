9b9ef94fa323bcda277917784c8b8c0de759982a
Richard Neumann <r.neumann@homeinfo.de>
2023-02-27 10:42:45 +0100

    Add canonical_name

--- dsla/datastructures/selection_method.py
+++ dsla/datastructures/selection_method.py
@@ -15,6 +15,23 @@ class SelectionMethod(str, Enum):
     MAHALANOBIS = 'mahalanobis'
     TWO_SIDED = 'two-sided'
 
+    @property
+    def canonical_name(self) -> str:
+        """Returns the canonical name for display."""
+        if self is (cls := type(self)).LASSO:
+            return 'lasso'
+
+        if self is cls.BRUSH:
+            return 'brush strokes'
+
+        if self is cls.MAHALANOBIS:
+            return 'Mahalanobis brush'
+
+        if self is cls.TWO_SIDED:
+            return 'two-sided brush'
+
+        raise NotImplementedError(f'No canonical name for {self}')
+
     @classmethod
     def from_string(cls, text: str) -> SelectionMethod:
         """Create a SelectionMethod from the given text."""
