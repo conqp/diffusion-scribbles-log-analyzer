505768fbd87f29312926586fb30c10b7a1d42962
Richard Neumann <r.neumann@homeinfo.de>
2023-02-27 17:32:23 +0100

    Update TLX plot

--- dsla/plots/nasa_tlx.py
+++ dsla/plots/nasa_tlx.py
@@ -22,46 +22,13 @@ STATEMENTS_SHORT = [
 def plot_nasa_tlx(experiments: list[Experiment], offset: float = -0.3) -> None:
     """Plots the raw and weighted NASA-TLX distributions."""
 
-    plot_raw_nasa_tlx(experiments, offset=offset)
-    plot_weighted_nasa_tlx(experiments, offset=offset)
+    _plot_nasa_tlx(experiments, 'raw', offset=offset)
+    _plot_nasa_tlx(experiments, 'weighted', offset=offset)
 
 
-def plot_raw_nasa_tlx(
+def _plot_nasa_tlx(
         experiments: list[Experiment],
-        offset: float = -0.3
-) -> None:
-    """Plot the raw NASA-TLX averages."""
-
-    for index, (method, nasa_tlx) in enumerate(
-            average_tlx(experiments)['methods'].items()
-    ):
-        y = list(nasa_tlx['normalized'].values())
-        x = range(1, len(y) + 1)
-        pyplot.bar(
-            [p + offset + index * 0.2 for p in x],
-            y,
-            0.2,
-            label=SelectionMethod(method).canonical_name
-        )
-
-    pyplot.xticks(
-        x,
-        [
-            f'{short_desc} ({index})'
-            for index, short_desc in enumerate(STATEMENTS_SHORT, start=1)
-        ],
-        rotation=-45,
-        ha='left'
-    )
-    pyplot.subplots_adjust(bottom=0.3)
-    pyplot.title('Average raw NASA-TLX results')
-    pyplot.ylabel('Score')
-    pyplot.legend(loc='center')
-    pyplot.show()
-
-
-def plot_weighted_nasa_tlx(
-        experiments: list[Experiment],
+        key: str,
         offset: float = -0.3
 ) -> None:
     """Plot the weighted NASA-TLX averages."""
@@ -69,8 +36,8 @@ def plot_weighted_nasa_tlx(
     for index, (method, nasa_tlx) in enumerate(
             average_tlx(experiments)['methods'].items()
     ):
-        y = list(nasa_tlx['weighted'].values())
-        x = range(1, len(y) + 1)
+        y = list(nasa_tlx[key].values())
+        x = range(len(y))
         pyplot.bar(
             [p + offset + index * 0.2 for p in x],
             y,
@@ -79,13 +46,13 @@ def plot_weighted_nasa_tlx(
         )
 
     pyplot.xticks(
-        x,
-        [
-            f'{index} ({short_desc})'
-            for index, short_desc in enumerate(STATEMENTS_SHORT, start=1)
-        ]
+        range(len(STATEMENTS_SHORT)),
+        STATEMENTS_SHORT,
+        rotation=-45,
+        ha='left'
     )
-    pyplot.title('Average weighted NASA-TLX results')
+    pyplot.subplots_adjust(bottom=0.25)
+    pyplot.title(f'Average {key} NASA-TLX results')
     pyplot.ylabel('Score')
-    pyplot.legend(loc='center')
+    pyplot.legend(loc='upper left')
     pyplot.show()
